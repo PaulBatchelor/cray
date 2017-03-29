06dc1a0442ad4d494aedaff56d6ef6174f98163b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-03-28 20:33:20 -0700

    added vec3.h

---
+++ vec3.h
@@ -0,0 +1,16 @@
+#ifndef VEC3H
+#define VEC3H
+
+#ifndef CRAYFLT
+#define CRAYFLT float
+#endif
+
+typedef struct {
+    CRAYFLT x;
+    CRAYFLT y;
+    CRAYFLT z;
+} vec3;
+
+# define VEC3_SET(V, X, Y, Z) (V).x = (X); (V).y = (Y); (V).z = (Z);
+
+#endif
