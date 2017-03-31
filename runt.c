23b98cb139f4e46ba5610b04ae3f784844bca213
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-03-30 18:28:41 -0700

    more runt words

--- runt.c
+++ runt.c
@@ -39,12 +39,10 @@ static runt_int rproc_cray_init(runt_vm *vm, runt_ptr p)
     w = s->f;
 
     cray_scene_init(scene, w, h, samps, 0);
-/*
     runt_malloc(vm, sizeof(cray_object *) * nobj, (void **)&obj);
     runt_mark_set(vm);
 
     cray_hitablelist_init(&scene->world, obj, nobj);
-*/
     return RUNT_OK;
 }
 
@@ -65,6 +63,83 @@ static runt_int rproc_write_ppm(runt_vm *vm, runt_ptr p)
     cray_scene_render(scene, 0, 0, scene->nx, scene->ny, buf, 1);
     cray_scene_write_ppm(scene, filename, buf);
     free(buf);
+    return RUNT_OK;
+}
+
+static runt_int rproc_add_sphere(runt_vm *vm, runt_ptr p)
+{
+    cray_sphere *sphere;
+    cray_scene *scene;
+    runt_float x, y, z;
+    runt_float radius;
+    runt_int rc;
+    runt_stacklet *s;
+    cray_material *mat;
+
+    scene = runt_to_cptr(p);
+    
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    mat = runt_to_cptr(s->p);
+
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    radius = s->f;
+    
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    z = s->f;
+    
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    y = s->f;
+    
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    x = s->f;
+
+    runt_malloc(vm, sizeof(cray_sphere), (void **)&sphere);
+
+
+    cray_sphere_init(sphere, mat);
+    cray_sphere_pos(sphere, x, y, z);
+    cray_sphere_radius(sphere, radius);
+    
+    cray_hitablelist_append(&scene->world, &sphere->obj);
+
+    runt_mark_set(vm);
+    return RUNT_OK;
+}
+
+static runt_int rproc_lambertian(runt_vm *vm, runt_ptr p)
+{
+    runt_int rc;
+    runt_stacklet *s;
+    cray_lambertian *lam;
+    runt_float r, g, b;
+
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    b = s->f;
+    
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    g = s->f;
+    
+    rc = runt_ppop(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+    r = s->f;
+
+    runt_malloc(vm, sizeof(cray_lambertian), (void **)&lam);
+    runt_mark_set(vm);
+    cray_lambertian_init(lam);
+    cray_lambertian_color(lam, r, g, b);
+   
+    rc = runt_ppush(vm, &s);
+    RUNT_ERROR_CHECK(rc);
+
+    s->p = runt_mk_cptr(vm, &lam->mat);
+
     return RUNT_OK;
 }
 
@@ -90,6 +165,8 @@ runt_int runt_load_cray(runt_vm *vm)
 
     cray_define(vm, "cray_init", 9, rproc_cray_init, p);
     cray_define(vm, "write_ppm", 9, rproc_write_ppm, p);
+    cray_define(vm, "add_sphere", 10, rproc_add_sphere, p);
+    cray_define(vm, "lambertian", 10, rproc_lambertian, p);
 
     return RUNT_OK;
 }
